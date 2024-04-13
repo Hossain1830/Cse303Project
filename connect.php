3090ba09effa165b8da0fb97b3e305ac899f58ce
Hossain1830 <150835507+Hossain1830@users.noreply.github.com>
2024-04-13 22:44:06 +0600

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,38 +1,42 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "test";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $role = $_POST["role"];
-    $name = $_POST["name"];
-
-    if ($role == "doctor") {
-        $specialist = $_POST["specialist"];
-        $phone_number = $_POST["phone_number"];
-
-        $sql = "INSERT INTO `doctor_t` (`name`, `specialist`, `phone_number`) VALUES ('$name', '$specialist', '$phone_number')";
-    }
-    elseif ($role == "patient") {
-        $gender = $_POST["gender"];
-        $disease = $_POST["disease"];
-        $age = $_POST["age"];
-        $sql = "INSERT INTO `patient_t` (`name`, `gender`, `disease`, `age`) VALUES ('$name', '$gender', '$disease', '$age')";
-    }
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+$role = $_POST["role"];
+
+if ($role == "doctor") {
+    $name = $_POST["dname"];
+    $specialist = $_POST["specialist"];
+    $phone_number = $_POST["phone_number"];
+
+    // Insert data into doctor_t table
+    $sql = "INSERT INTO `doctor_t` (`d_id`, `dname`, `specialist`, `phone_number`) VALUES (NULL, '$name', '$specialist', '$phone_number')";
+} elseif ($role == "patient") {
+    $name = $_POST["name"];
+    $gender = $_POST["gender"];
+    $disease = $_POST["disease"];
+    $age = $_POST["age"];
+
+    // Insert data into patient_t table
+    $sql = "INSERT INTO `patient_t` (`p_id`, `name`, `gender`, `disease`, `age`) VALUES (NULL, '$name', '$gender', '$disease', '$age')";
+}
+
+// Execute SQL query
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
